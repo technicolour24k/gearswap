b313362ef74ebcecea5fd0379e8a6025add99736
TonyJ <hitetsu@hotmail.com>
2022-01-09 17:50:47 +0000

    Updated oneClickRemedies function
    - Checks inventory and relays med count back

--- data/includes/common-functions.lua
+++ data/includes/common-functions.lua
@@ -82,14 +82,45 @@ function clearStatuses()
 	local remedyOintmentListLength = table.getn(remedyOintment_list)
 	local panaceaListLength = table.getn(panacea_list)
 	local holyWaterListLength = table.getn(holyWater_list)
-
+	local remedyCount = 0
+	local remedyOintmentCount = 0
+	local panaceaCount = 0
+	local holyWaterCount = 0
+	local hallowedWaterCount = 0
+	
 	-- Fail safe in case this accidentally triggers. Should be checked by the calling LUA. 
-	if (config.oneClickRemedies) then		
+	if (config.oneClickRemedies) then
+		local invTable = windower.ffxi.get_bag_info(0) --Grab the contents of my bag
+		local invItemCount = windower.ffxi.get_items(0) --Grab the number of items in bag
+		n = 0
+		while (n < (invTable.max +1)) do
+			local itemInfo = windower.ffxi.get_items(0, n)
+			local itemCount = itemInfo.count
+			count = 1
+			while (count <= 5) do  --Cycle through items and count up Remedies, Ointments, Panacea, and Holy/Hallowed Water
+				if (itemInfo.id == 4155) then
+					remedyCount = itemInfo.count
+				elseif (itemInfo.id == 5356) then
+					remedyOintmentCount = itemInfo.count
+				elseif (itemInfo.id == 4149) then
+					panaceaCount = itemInfo.count
+				elseif (itemInfo.id == 4154) then
+					holyWaterCount = itemInfo.count
+				elseif (itemInfo.id == 5306) then
+					hallowedWaterCount = itemInfo.count
+				end
+				count = count+1
+			end
+			n = n+1
+		end
+
 		for i=1, holyWaterListLength do
 			if (buffactive[holyWater_list[i]]) then
 				add_to_chat(38, holyWater_list[i].." found in buffactive")
 				send_command('input /item "Hallowed Water" <me>')
 				send_command('input /item "Holy Water" <me>')
+				add_to_chat(10, "Holy Waters remaining: " ..holyWaterCount)
+				add_to_chat(10, "Hallowed Waters remaining: " ..hallowedWaterCount)
 				break
 			end
 		end
@@ -99,6 +130,8 @@ function clearStatuses()
 				add_to_chat(38, remedy_list[i].." found in buffactive")
 				send_command('input /item "Remedy" <me>')
 				send_command('input /item "Remedy Ointment" <me>')
+				add_to_chat(10, "Remedy remaining: " ..remedyCount)
+				add_to_chat(10, "Remedy Ointment remaining: " ..remedyOintmentCount)
 				break
 			end
 		end
@@ -107,6 +140,7 @@ function clearStatuses()
 			if (buffactive[remedy_list[i]]) then
 				add_to_chat(38, remedy_list[i].." found in buffactive")
 				send_command('input /item "Remedy Ointment" <me>')
+				add_to_chat(10, "Remedy Ointment remaining: " ..remedyOintmentCount)
 				break
 			end
 		end
@@ -115,6 +149,7 @@ function clearStatuses()
 			if (buffactive[remedy_list[i]]) then
 				add_to_chat(38, remedy_list[i].." found in buffactive")
 				send_command('input /item "Panacea" <me>')
+				add_to_chat(10, "Panacea remaining: " ..panaceaCount)
 				break
 			end
 		end
