ebc0f013429c33bf176bd670f68281ea3d26b0f2
technicolour24k <35193590+technicolour24k@users.noreply.github.com>
2022-01-09 22:31:19 +0000

    Delete export directory

--- data/export/Kin 2021-05-23 20-45-38.lua
+++
@@ -1,9 +0,0 @@
-sets.exported={
-    head="Mochi. Hatsuburi +1",
-    body="Gyve Doublet",
-    hands="Hattori Tekko +1",
-    neck="Stoicheion Medal",
-    waist={ name="Windbuffet Belt +1", augments={'Crit.hit rate+5','"Triple Atk."+2','"Triple Atk."+2','"Triple Atk."+2',}},
-    left_ear={ name="Hecate's Earring", augments={'System: 1 ID: 131 Val: 7','System: 1 ID: 131 Val: 7','System: 1 ID: 131 Val: 7','System: 1 ID: 131 Val: 7',}},
-    right_ear={ name="Sortiarius Earring", augments={'System: 1 ID: 131 Val: 7','System: 1 ID: 131 Val: 7','System: 1 ID: 131 Val: 7','System: 1 ID: 131 Val: 7',}},
-}
