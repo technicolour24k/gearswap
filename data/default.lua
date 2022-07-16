de09712f6d2e1173d8b7c13ef4637ca16aa04dd8
Tony Jordan <hitetsu@hotmail.com>
2022-07-16 09:11:29 +0100

    Level check didn't work
    - Removed generic.lua check and use from default.lua
    - Check was too flaky

--- data/default.lua
+++ data/default.lua
@@ -1,9 +1,11 @@
 include('includes/config')
+include('includes/common-functions')
 
 function get_sets()
-	send_command("echo Clearing all keybinds..")
+	
+	debugLog("Clearing all keybinds..")
 	send_command('clearbinds')
-	send_command("echo Clearing all aliases..")
+	debugLog("Clearing all aliases..")
 	send_command('clearaliases')
 	send_command('clear')
 	send_command('wait 0.2; echo Dependency: "run" plugin - attempting to load.')
@@ -11,23 +13,16 @@ function get_sets()
 	send_command('run addons/gearswap/data/copy-scripts.bat')
 	send_command("wait 0.2; echo Executing Windower key binds..")
 	send_command('exec windower-bindings')
-	send_command("echo Executing "..player.main_job.." key binds..")
+	debugLog("Executing "..player.main_job.." key binds..")
 	send_command('exec "binds/common"')
 	send_command('exec "binds/'..server..'/'..player.name..'_'..player.main_job..'"')
-	send_command("echo Executing alias setup..") 
+	debugLog("Executing alias setup..") 
 	send_command('exec "aliases/common"')
 	send_command('exec "aliases/'..server..'/'..player.name..'_'..player.main_job..'"')
 	
 	if server ~= "retail" then
-		if (player.main_job_level < 99) then
-			send_command('wait 1;gs load "private servers/'..server..'/generic"')
-		else
 			send_command('wait 1;gs load "private servers/'..server..'/'..player.name..'_'..player.main_job..'"')
-		end
 	else
-		if (player.main_job_level < 99) then
-			send_command('wait 1;gs load "retail/generic"')
-		else
 			send_command('wait 1;gs load "retail/'..player.name..'_'..player.main_job..'"')
-		end	end
+	end
 end
