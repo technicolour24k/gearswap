0e209e89fd2991c97678283922409f0e302ee96d
Tony Jordan <hitetsu@hotmail.com>
2022-08-24 19:44:13 +0100

    Draft: Status removal refactor

--- data/includes/common-functions.lua
+++ data/includes/common-functions.lua
@@ -1,5 +1,7 @@
 include('includes/config')
 include('includes/player-stats')
+res = require('resources')
+
 
 --initialise variables to inherit from master config
 local showFCInfo = config.showFastCastInfo
@@ -88,86 +90,30 @@ function spellContains(spell, contains)
 	end
 end
 
-function clearStatuses()	
-	local remedyListLength = table.getn(remedy_list)
-	local remedyOintmentListLength = table.getn(remedyOintment_list)
-	local panaceaListLength = table.getn(panacea_list)
-	local holyWaterListLength = table.getn(holyWater_list)
-	local remedyCount = 0
-	local remedyOintmentCount = 0
-	local panaceaCount = 0
-	local holyWaterCount = 0
-	local hallowedWaterCount = 0
-
-	add_to_chat(8, "Clear Status function called")
+ function clearStatuses()	
+ 	local remedyListLength = table.getn(remedy_list)
+ 	local remedyOintmentListLength = table.getn(remedyOintment_list)
+ 	local panaceaListLength = table.getn(panacea_list)
+ 	local holyWaterListLength = table.getn(holyWater_list)
 	
-	-- Fail safe in case this accidentally triggers. Should be checked by the calling LUA. 
-	if (config.oneClickRemedies) then
-		local invTable = windower.ffxi.get_bag_info(0) --Grab the contents of my bag
-		local invItemCount = windower.ffxi.get_items(0) --Grab the number of items in bag
-		n = 0
-		while (n < (invTable.max +1)) do
-			local itemInfo = windower.ffxi.get_items(0, n)
-			local itemCount = itemInfo.count
-			count = 1
-			while (count <= 5) do  --Cycle through items and count up Remedies, Ointments, Panacea, and Holy/Hallowed Water
-				if (itemInfo.id == 4155) then
-					remedyCount = itemInfo.count
-				elseif (itemInfo.id == 5356) then
-					remedyOintmentCount = itemInfo.count
-				elseif (itemInfo.id == 4149) then
-					panaceaCount = itemInfo.count
-				elseif (itemInfo.id == 4154) then
-					holyWaterCount = itemInfo.count
-				elseif (itemInfo.id == 5306) then
-					hallowedWaterCount = itemInfo.count
-				end
-				count = count+1
-			end
-			n = n+1
-		end
 
-		for i=1, holyWaterListLength do
-			if (buffactive[holyWater_list[i]]) then
-				add_to_chat(38, holyWater_list[i].." found in buffactive")
-				send_command('input /item "Hallowed Water" <me>')
-				send_command('input /item "Holy Water" <me>')
-				add_to_chat(10, "Holy Waters remaining: " ..holyWaterCount)
-				add_to_chat(10, "Hallowed Waters remaining: " ..hallowedWaterCount)
-				break
-			end
-		end
+ 	add_to_chat(8, "Clear Status function called")
 		
-		for i=1, remedyListLength do
-			if (buffactive[remedy_list[i]]) then
-				add_to_chat(38, remedy_list[i].." found in buffactive")
-				send_command('input /item "Remedy" <me>')
-				send_command('input /item "Remedy Ointment" <me>')
-				add_to_chat(10, "Remedy remaining: " ..remedyCount)
-				add_to_chat(10, "Remedy Ointment remaining: " ..remedyOintmentCount)
-				break
-			end
+ 	-- Fail safe in case this accidentally triggers. Should be checked by the calling LUA. 
+ 	if (config.oneClickRemedies) then
+		if buffactive['silence']  then
+			windower.send_command('input /item "Echo Drops" '..windower.ffxi.get_player()["name"])
+		elseif (buffactive:contains(remedy_list)) then
+			windower.send_command('input /item "Remedy" '..windower.ffxi.get_player()["name"])
+		elseif (buffactive:contains(remedyOintment_list)) then
+			windower.send_command('input /item "Remedy Ointment" '..windower.ffxi.get_player()["name"])
+		elseif (buffactive:contains(panacea_list)) then
+			windower.send_command('input /item "Panacea" '..windower.ffxi.get_player()["name"])
+		elseif (buffactive:contains(holyWater_list)) then
+			windower.send_command('input /item "Hallowed Water" '..windower.ffxi.get_player()["name"])
 		end
-		
-		for i=1, remedyOintmentListLength do
-			if (buffactive[remedyOintment_list[i]]) then
-				add_to_chat(38, remedyOintment_list[i].." found in buffactive")
-				send_command('input /item "Remedy Ointment" <me>')
-				add_to_chat(10, "Remedy Ointment remaining: " ..remedyOintmentCount)
-				break
-			end
-		end
-		
-		for i=1, panaceaListLength do
-			if (buffactive[panacea_list[i]]) then
-				add_to_chat(38, panacea_list[i].." found in buffactive")
-				send_command('input /item "Panacea" <me>')
-				add_to_chat(10, "Panacea remaining: " ..panaceaCount)
-				break
-			end
-		end
-	end
-end
+ 	end
+ end
 
 function echoInfo(info,delay)
 	if (delay) then
@@ -202,3 +148,24 @@ function announceSpell(spell,target, chatmode)
 		send_command('input /'..chatmode..' '..spell..' => '..target)
 	end
 end
+
+
+windower.register_event('gain buff', function(id)
+    local name = res.buffs[id].english
+	local combinedWatchList = {"Blind","Paralysis","Silence","Poison", "Disease", "Plague", "Bind", "Bio", "Burn", "Choke", "Dia", "Flash", "Frost", "Gravity", "Rasp", "Slow", "Stun", "Weight", "Attack Down", "Defense Down", "Curse","Doom"}
+    for key,val in pairs(combinedWatchList) do
+        if val:lower() == name:lower() then
+            if name:lower() == 'silence'  then
+                windower.send_command('input /item "Echo Drops" '..windower.ffxi.get_player()["name"])
+            elseif (remedy_list:contains(name:lower())) then
+                windower.send_command('input /item "Remedy" '..windower.ffxi.get_player()["name"])
+            elseif (remedyOintment_list:contains(name:lower())) then
+                windower.send_command('input /item "Remedy Ointment" '..windower.ffxi.get_player()["name"])
+            elseif (panacea_list:contains(name:lower())) then
+                windower.send_command('input /item "Panacea" '..windower.ffxi.get_player()["name"])
+            elseif (holyWater_list:contains(name:lower())) then
+                windower.send_command('input /item "Hallowed Water" '..windower.ffxi.get_player()["name"])
+            end
+        end
+    end
+end)
